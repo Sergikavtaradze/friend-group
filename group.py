491393ba0df1e87c3f50b61f8398d18019c6d71b
Sergi Kavtaradze <20skavtaradze@gmail.com>
2024-10-14 11:51:04 +0100

    adding some print statements about our database

--- group.py
+++ group.py
@@ -1,4 +1,6 @@
 import copy
+import statistics
+import numpy as np
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
@@ -27,8 +29,18 @@ def forget(storage, person1, person2):
     
     return deep_copied
 
+# Add some code that makes use of comprehension expressions to your group.py file so that it prints out the following when the script is run:
+# the maximum age of people in the group
+# the average (mean) number of relations among members of the group
+# the maximum age of people in the group that have at least one relation
+# [more advanced] the maximum age of people in the group that have at least one friend
 
+max_age = max([values["age"] for name, values in my_group.items()])
+avg_relations = np.mean([len(values["relationship"]) for name, values in my_group.items()])
+max_age_w_friend = max([values["age"] for name, values in my_group.items() if len(values["relationship"]["friends"]) > 0])
+
+print(f'This is the max age {max_age}, the mean relations {avg_relations}, blabla {max_age_w_friend}')
 
 # Jill is 26, a biologist and she is Zalika's friend and John's partner.
-for row in my_group:
-    print(f'{row["name"]} is {row["age"]}, (s)he is a {row["job"]} and he is friends with {row["relationship"]["friends"]} and in a romantic relationship with {row["relationship"]["partner"]}')
+# for row in my_group:
+#     print(f'{row["name"]} is {row["age"]}, (s)he is a {row["job"]} and he is friends with {row["relationship"]["friends"]} and in a romantic relationship with {row["relationship"]["partner"]}')
